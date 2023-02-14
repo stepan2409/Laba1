c9941a5004ab05bfb93d144c2c6da011858dc148
Chesnokov Stepan <stepan.chesnokov@gmail.com>
2023-02-14 14:07:51 +0300

    Добавьте файлы проекта.

---
+++ Laba1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.invitationLabel = new System.Windows.Forms.Label();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // invitationLabel
+            // 
+            this.invitationLabel.AutoSize = true;
+            this.invitationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.invitationLabel.Location = new System.Drawing.Point(13, 12);
+            this.invitationLabel.Name = "invitationLabel";
+            this.invitationLabel.Size = new System.Drawing.Size(112, 18);
+            this.invitationLabel.TabIndex = 0;
+            this.invitationLabel.Text = "Введите число";
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numberTextBox.Location = new System.Drawing.Point(12, 33);
+            this.numberTextBox.MaxLength = 10;
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(372, 24);
+            this.numberTextBox.TabIndex = 1;
+            this.numberTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.calculateButton.Location = new System.Drawing.Point(12, 64);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(372, 28);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "ВЫЧИСЛИТЬ";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel.Location = new System.Drawing.Point(12, 99);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 20);
+            this.resultLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(403, 151);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.invitationLabel);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 1-1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label invitationLabel;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Laba1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Laba1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            int enteredNumber = getNumberFromInput();
+            if (enteredNumber == 0)
+            {
+                return;
+            }
+            resultLabel.Text = createAnswerString(getMinimalDigits(enteredNumber));
+        }
+
+        private (int, int) getMinimalDigits(int number)
+        {
+            int a = number / 100;
+            int b = number / 10 % 10;
+            int c = number % 10;
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    return (b, c);
+                } else
+                {
+                    return (a, b);
+                }
+            } else
+            {
+                if (b > c)
+                {
+                    return (a, c);
+                } else
+                {
+                    return (a, b);
+                }
+            }
+        }
+
+        private string createAnswerString((int, int) minimalNumbers)
+        {
+            string answer = "Сумма двух наименьших цифр:\n";
+            answer += minimalNumbers.Item1.ToString() + " + " + minimalNumbers.Item2.ToString() + " = ";
+            answer += (minimalNumbers.Item1 + minimalNumbers.Item2).ToString();
+            return answer;
+        }
+
+        private int getNumberFromInput()
+        {
+            if (this.numberTextBox.TextLength > 3)
+            {
+                this.invitationLabel.Text = "Слишком длинно! Ожидалось 3 цифры!";
+                this.invitationLabel.ForeColor = Color.Red;
+                return 0;
+            }
+            if (this.numberTextBox.TextLength < 3)
+            {
+                this.invitationLabel.Text = "Слишком коротко! Ожидалось 3 цифры!";
+                this.invitationLabel.ForeColor = Color.Red;
+                return 0;
+            }
+
+            uint enteredNumber;
+            if (!uint.TryParse(this.numberTextBox.Text, out enteredNumber))
+            {
+                this.invitationLabel.Text = "Только цифры!";
+                this.invitationLabel.ForeColor = Color.Red;
+                return 0;
+            }
+
+            if (enteredNumber < 100)
+            {
+                this.invitationLabel.Text = "Слишком коротко! Нужно 3 ЗНАЧАЩИХ цифры!";
+                this.invitationLabel.ForeColor = Color.Red;
+                return 0;
+            }
+
+            this.invitationLabel.Text = "Введите число";
+            this.invitationLabel.ForeColor = Color.Black;
+            return (int)enteredNumber;
+        }
+    }
+}
